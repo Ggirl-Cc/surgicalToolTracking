d09d72a9a3e3dc335eb28d03eef46238a1d35cee
geetika_o <110749599+Ggirl-Cc@users.noreply.github.com>
2026-08-14 16:24:17 -0700

    Clean up project structure

--- src/download_cholectrack20.py
+++ src/download_cholectrack20.py
@@ -1,6 +1,7 @@
 import requests
 import synapseutils
 import synapseclient
+from synapse_config import email, authToken, accesskey
 
 
 def main():
@@ -23,9 +24,5 @@ def main():
 
 
 if __name__ == "__main__":
-    email = "geetikasahasra@gmail.com"
-    authToken = "eyJ0eXAiOiJKV1QiLCJraWQiOiJXN05OOldMSlQ6SjVSSzpMN1RMOlQ3TDc6M1ZYNjpKRU9VOjY0NFI6VTNJWDo1S1oyOjdaQ0s6RlBUSCIsImFsZyI6IlJTMjU2In0.eyJhY2Nlc3MiOnsic2NvcGUiOlsidmlldyIsImRvd25sb2FkIl0sIm9pZGNfY2xhaW1zIjp7fX0sInRva2VuX3R5cGUiOiJQRVJTT05BTF9BQ0NFU1NfVE9LRU4iLCJpc3MiOiJodHRwczovL3JlcG8tcHJvZC5wcm9kLnNhZ2ViYXNlLm9yZy9hdXRoL3YxIiwiYXVkIjoiMCIsIm5iZiI6MTc4NDE0MzUxOSwiaWF0IjoxNzg0MTQzNTE5LCJqdGkiOiI0MjI2NCIsInN1YiI6IjM2MDMwODQifQ.IRN4XLFfjZc4s4DgcEKLeMGa5sdSzFRxi-6nEBPArKHtol4_LLzCGBK4B_eriVWKE40-zino4ZZ5FRDT2v_bTWrrclHfgATocjf1i4qWgCXzBDaBNVjoR6VkSK6mGeeLD_dEUtOEeggM10HVXeCnTI4kQotitLJ3uejbluvyLui6odq04MgbJryraDfmdsXYZ3krt4fM6PMi-PNJlm9U6YngEWUj-kgAcobS6n9uJUjNZ_iPbuX9dAnej3LzSBiUcndcfVekm-dxpM00Stwg-hHTgSw22xLGD4st1NVdK0IlQncNNb_-gjwKR6FAkippKH3in1uyPeLfRgFefhQ_Ew"
-    accesskey = "NTIJTFK.4194711"
     local_folder = r"data\CholecTrack20"
-
     main()
